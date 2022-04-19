eb8df6cd6cba4f58aadeae3331d6a4a6b9cb8dcd
Entsllor <slava90100@yandex.com>
2022-04-19 17:41:06 +0500

    style: paint paginator and bg

--- frontend/app/src/components/paginator/Paginator.tsx
+++ frontend/app/src/components/paginator/Paginator.tsx
@@ -6,8 +6,8 @@ const Paginator: React.FC<{setPage: CallableFunction, page: number}> = (props) =
         <ul className="pagination">
             {props.page > 1 ? <li className="page-item"><button className="page-link" onClick={() => props.setPage(props.page - 1)}>{"<"}</button></li> : null}
             {props.page > 1 ? <li className="page-item"><button className="page-link" onClick={() => props.setPage(1)}>1</button></li> : null}
-            <li className="page-item"><div className="page-link fw-bold">{props.page}</div></li>
-            <li className="page-item"><button className="page-link" onClick={() => props.setPage(props.page + 1)}>{">"}</button></li>
+            <li className="page-item"><div className="page-link link-success fw-bold">{props.page}</div></li>
+            <li className="page-item"><button className="page-link link-success" onClick={() => props.setPage(props.page + 1)}>{">"}</button></li>
         </ul>
     </div>
 }
